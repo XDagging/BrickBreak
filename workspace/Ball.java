91c6b6bfa8e88bad495d541e1a42130e28649e65
XDagging <113935822+XDagging@users.noreply.github.com>
2024-12-05 19:11:30 +0000

    commit -m “adsfasdf”

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,11 +1,91 @@
 import java.awt.*;
 
+//  Class author:  Sebastian Hernandez-Tavares
+//  Date created:  12/5/24
+//  General description: Creates the ball class that represents the ball in the game brickbreak. This class is dependent on other files in this same folder. 
+
 public class Ball {
 	//your code here!
 	
 	//don't forget you need instance variables:
 	
+
+	private int xPos;
+    private int yPos;
+	private int radius;
+   	private int velocityX;
+	private int velocityY;
+
+	public Ball(int x, int y , int w) {
+		xPos = x;
+		yPos = y;
+		radius = w;
+	}
+
+	//  Pre-condition: Needs a instance of the Graphics class as a parameter to be sent as a parameter.
+	//  Post-condition: Redraws the instance with the xPos yPos and the width and height. 
+	public void draw(Graphics f) {
+
+		f.setColor(Color.orange);
+
+		f.fillOval(xPos, yPos, radius, radius);
+	}
 	
+
+    
+	public int getYpos() {
+		return yPos;
+	}	
+
+	public int getXpos() {
+		return xPos;
+	}	
+
+	public int getSize() {
+		return radius;
+	}
+
+	public void setXVelocity(int velocity) {
+		velocityX = velocity;
+	}
+	public void setYVelocity(int velocity) {
+		velocityY = velocity;
+	}
+
+
+	//  Pre-condition: None
+	//  Post-condition: Adds the velocities for both x and y to the xposition and yposition.
+
+	public void move(){
+		xPos+=velocityX;
+		yPos+=velocityY;
+	}
+
+	//  Pre-condition: None
+	//  Post-condition: Will make instance attribute (velocityY) inverse aka go the other way
+	public void reverseY() {
+		velocityY *= -1;
+	}
+
+
+	//  Pre-condition: None
+	//  Post-condition: Will make instance attribute (velocityX) inverse aka go the other way
+	public void reverseX() {
+		velocityX *= -1;
+	}
+
+	public void setX(int value) {
+		xPos = value;
+	}
+	public void setY(int value) {
+		yPos = value;
+	}
+
+
+	
+
+
+
 	//constructor(s):
 	
 	//methods:
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,4 +1,8 @@
-import java.awt.Graphics;
+//  Class author:  Sebastian Hernandez-Tavares
+//  Date created:  12/5/24
+//  General description: Creates the paddle that can move to the left or to the right depending on the methods called upon the class.
+
+
 import java.awt.*;
 
 public class Paddle {
@@ -6,9 +10,75 @@ public class Paddle {
 	//your code here!
 	
 	//don't forget you need instance variables:
+	private int xPos;
+	private int yPos;
+	private int height;
+	private int width;
+	private int velocity;
 	
+
+	// player = new Paddle(310, 550, 100, 8);
 	
-	//constructor(s):
+	public Paddle(int x, int y, int w, int h) {
+		xPos = x;
+		yPos = y;
+		height = h;
+		width = w;
+	}
+
+
+	//  Pre-condition: Needs a instance of the Graphics class as a parameter to be sent as a parameter.
+	//  Post-condition: Redraws the instance with the xPos yPos and the width and height. 
+	public void draw(Graphics f) {
+
+		
+
+		f.setColor(Color.white);
+		f.fillRect(xPos, yPos, width, height);
+	}
+
+	public int getX() {
+		return xPos;
+	}
+
+	public void setX(int value) {
+		xPos = value;
+	}
+
+	public int getY() {
+		return yPos;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getVelocity() {
+		return velocity;
+	}
+	public void addVelocity(int add) {
+		velocity = add;
+	}
+
+
+
+	//  Pre-condition: None
+	//  Post-condition: Adds the velocity to the xPos.
+	public void move() {
+		xPos += velocity;
+	}
+
+	public void setVelocity(int newVelocity) {
+		velocity = newVelocity;
+	}
+	// draw(Graphics)
+
+
+
 	
 	//methods:
 	
